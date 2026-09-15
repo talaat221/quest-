aa2f402e4772a425b5aadaf51d66976ffad607bb
talaat221 <150787768+talaat221@users.noreply.github.com>
2026-09-16 00:15:45 +0300

    Merge iPhone UI
    Merge the finalized iPhone responsive layout and mobile modal polish into main.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './iphone-pass2.css'
+import './iphone-modal-fix.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
