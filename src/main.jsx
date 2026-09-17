08b1b903f5a9a1562dde71fc23a9f9b80f824f48
talaat221 <150787768+talaat221@users.noreply.github.com>
2026-09-17 22:03:33 +0300

    Enable pixel farm home on mobile

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './iphone-pass2.css'
 import './iphone-modal-fix.css'
+import './pixel-home.css'
+import './pixelHomeAssets.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
