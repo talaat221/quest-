1695fa03d6872c2f6e98d0c927af0da0af028ed9
talaat221 <150787768+talaat221@users.noreply.github.com>
2026-09-17 22:28:13 +0300

    Use safe pixel home renderer

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import './iphone-pass2.css'
 import './iphone-modal-fix.css'
 import './pixel-home-real.css'
-import './pixelHomeReal.js'
+import './pixelHomeRealSafe.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
